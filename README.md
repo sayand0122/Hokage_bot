6279c6355bae736648bd1600bd511b9a65c3fafb
Saatvik Ramani <saatvikramani777@gmail.com>
2020-12-26 10:56:00 +0530

    Comments for clarity and Readme update

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# Hokage_bot
+# Hokage Bot
+
+Fun bot project made with [discord.py](https://discordpy.readthedocs.io/en/latest/)
+
+### Setup
+
+To run this on your device, clone this repo and create a `secrets.py` file in the initial directory with your bot token as a string variable.
+
--- bot.py
+++ bot.py
@@ -10,9 +10,10 @@ bot = commands.Bot(command_prefix='.')
 
 @bot.event
 async def on_ready():
-    ''' 
-        Sends a message in terminal with start time 
-    '''
+    """
+        Sends a message in terminal with start time.
+        Primary use is just for logging.
+    """
     launch_time = datetime.now()
     print(f"Started at {launch_time}")
 
