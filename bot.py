2270de3b9c8990e694e269b254f2efe38904e87e
Saatvik Ramani <saatvikramani777@gmail.com>
2020-12-26 10:31:31 +0530

    boilerplate code

---
+++ bot.py
@@ -0,0 +1,20 @@
+import discord
+from discord.ext import commands
+
+from datetime import datetime
+
+from secrets import *
+
+bot = commands.Bot(command_prefix='.')
+
+
+@bot.event
+async def on_ready():
+    ''' 
+        Sends a message in terminal with start time 
+    '''
+    launch_time = datetime.now()
+    print(f"Started at {launch_time}")
+
+
+bot.run(TOKEN)
