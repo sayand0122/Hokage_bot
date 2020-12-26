6bf3ab814641b2a5a33343b482ef6a926e7d674e
Saatvik Ramani <saatvikramani777@gmail.com>
2020-12-26 11:44:01 +0530

    functionality to add cogs

--- bot.py
+++ bot.py
@@ -1,12 +1,17 @@
 import discord
 from discord.ext import commands
-import dotenv
 
 from datetime import datetime
-
+import dotenv
 from dotenv.main import load_dotenv
+import os
 
+"""
+    Loads environment variables from .env.
+    Initializes TOKEN as bot token.
+"""
 load_dotenv()
+TOKEN = os.environ.get('TOKEN')
 
 bot = commands.Bot(command_prefix='.')
 
@@ -19,6 +24,15 @@ async def on_ready():
     """
     launch_time = datetime.now()
     print(f"Started at {launch_time}")
+    
+
+for cog in os.listdir(r"./cogs"):
+    if cog.endswith(".py") and not cog.startswith("_"):
+        try:
+            cog = f"cogs.{cog.replace('.py', '')}"
+            bot.load_extension(cog)
+        except Exception as e:
+            print(f"{cog} can not be loaded\n{e}")
 
 
 bot.run(TOKEN)
