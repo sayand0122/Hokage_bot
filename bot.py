1fb485d1a6f067a753474f0a97539dc7909a6c8c
sayand0122 <sayandutta.jan@gmail.com>
2020-12-27 11:59:19 +0530

    update

--- bot.py
+++ bot.py
@@ -33,22 +33,6 @@ async def load(ctx, extension):
     bot.load_extension(f'cogs.{extension}')
 
 
-<<<<<<< HEAD
-@bot.command()
-async def unload(ctx, extension):
-    bot.unload_exntension(f'cogs.{extension}')
-
-
-for filename in os.listdir('./cogs'):
-    if filename.endswith('.py'):
-        bot.load_extension(f'cogs.{filename[:-3]}')
-
-
-# ping it and it returns your latency
-@bot.command()
-async def ping(ctx):
-    await ctx.send(f'Pong! {round(bot.latency * 1000)}ms')
-=======
 @bot.command(hidden=True)
 @commands.is_owner()
 async def reload(ctx, cog=None):
@@ -68,20 +52,6 @@ async def reload(ctx, cog=None):
         cog1 = f'cog `{cog}`'
 
     await ctx.send(f"Successfully reloaded {cog}")
-
-
-"""
-    Loads cogs from ./cogs directory.
-    Make sure your file name starts with '_' if you dont want it to load just yet. 
-"""
-for cog in os.listdir(r"./cogs"):
-    if cog.endswith(".py") and not cog.startswith("_"):
-        try:
-            cog = f"cogs.{cog.replace('.py', '')}"
-            bot.load_extension(cog)
-        except Exception as e:
-            print(f"{cog} can not be loaded\n{e}")
->>>>>>> main
 
 
 bot.run(DISCORD_TOKEN)
