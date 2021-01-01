5a130a404802240b498c59f7e73e4e0cc3fa5d56
Saatvik Ramani <saatvikramani777@gmail.com>
2021-01-01 23:23:41 +0530

    added embeds

--- bot.py
+++ bot.py
@@ -8,31 +8,27 @@ import os
 
 
 """
-    Loads environment variables from .env.
-    Initializes TOKEN as bot token.
+Loads environment variables from .env.
+Initializes TOKEN as bot token.
 """
 load_dotenv()
 TOKEN = os.environ.get('TOKEN')
 
 bot = commands.Bot(command_prefix=',')
 
-
 @bot.event
 async def on_ready():
     """
-        Sends a message in terminal with start time.
-        Primary use is just for logging.
+    Sends a message in terminal with start time.
+    Primary use is just for logging.
     """
     launch_time = datetime.now()
     print(f"Started at {launch_time}")
     
-
 @bot.command(hidden=True)
 @commands.is_owner()
 async def reload(ctx, cog=None):
-    """
-        Hot reloading of cogs.
-    """
+    """Hot reloading of cogs."""
     cog1 = None
     
     if cog is None:
@@ -48,10 +44,9 @@ async def reload(ctx, cog=None):
 
     await ctx.send(f"Successfully reloaded {cog1}")
     
-
 """
-    Loads cogs from ./cogs directory.
-    Make sure your file name starts with '_' if you dont want it to load just yet. 
+Loads cogs from ./cogs directory.
+Make sure your file name starts with '_' if you dont want it to load just yet. 
 """
 for cog in os.listdir(r"./cogs"):
     if cog.endswith(".py") and not cog.startswith("_"):
@@ -60,6 +55,5 @@ for cog in os.listdir(r"./cogs"):
             bot.load_extension(cog)
         except Exception as e:
             print(f"{cog} can not be loaded\n{e}")
-
 
 bot.run(TOKEN)
