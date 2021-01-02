f1750818ef4c2581b507abdf5053b517988564e0
Saatvik Ramani <saatvikramani777@gmail.com>
2021-01-02 11:34:06 +0530

    comments and imports

--- bot.py
+++ bot.py
@@ -1,34 +1,26 @@
 import discord
-import os
-import dotenv
-import pymongo
-
 from discord.ext import commands
+
+import os
+import pymongo
 from datetime import datetime
+import dotenv
 from dotenv.main import load_dotenv
 
 
-"""
-Loads environment variables from .env.
+"""Loads environment variables from .env.
 Initializes TOKEN as bot token.
 """
 load_dotenv()
 
 DISCORD_TOKEN = os.environ.get('TOKEN')
 
-
 bot = commands.Bot(command_prefix='.')
 
 @bot.event
 async def on_ready():
-    """
-    Sends a message in terminal with start time.
-    Primary use is just for logging.
-    """
     launch_time = datetime.now()
     print(f"Started at {launch_time}")
-
-    
 
 # clear commands and its error handling
 @bot.command()
@@ -43,8 +35,6 @@ async def clear_error(ctx, error):
         await ctx.send("Please pass the amount to clear the number of data.")
 
 # maintaining cogs
-
-
 @bot.command()
 async def load(ctx, extension):
     bot.load_extension(f'cogs.{extension}')
@@ -54,11 +44,9 @@ async def load(ctx, extension):
 async def unload(ctx, extension):
     bot.unload_exntension(f'cogs.{extension}')
 
-
 for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
         bot.load_extension(f'cogs.{filename[:-3]}')
 
 
 bot.run(DISCORD_TOKEN)
-
